c488fa8bc1e2b5f6fcaf07f94dcd4e3fb53244e2
luisftec <luisftec2022@gmail.com>
2023-11-26 02:15:31 -0500

    actualizacion de portfolio se cambio los fondos y se modifico cosas irrelevantes

--- scriptmenu/scriptnav.js
+++ scriptmenu/scriptnav.js
@@ -1,13 +1,12 @@
-    // JavaScript para ocultar/mostrar la barra de navegación
-    let ultimaPosicionScroll = window.pageYOffset; // Guarda la última posición del scroll
-    
-    window.onscroll = function() {
-        let posicionActualScroll = window.pageYOffset;
-        if (ultimaPosicionScroll > posicionActualScroll) {
-            document.getElementById("miBarraNavegacion").style.top = "0";
-        } else {
-            document.getElementById("miBarraNavegacion").style.top = "-60px"; // Ajusta este valor según la altura de tu barra de navegación
-        }
-        ultimaPosicionScroll = posicionActualScroll;
-    }
-    
+// JavaScript para ocultar/mostrar la barra de navegación
+let ultimaPosicionScroll = window.pageYOffset; // Guarda la última posición del scroll
+
+window.onscroll = function () {
+  let posicionActualScroll = window.pageYOffset;
+  if (ultimaPosicionScroll > posicionActualScroll) {
+    document.getElementById("miBarraNavegacion").style.top = "0";
+  } else {
+    document.getElementById("miBarraNavegacion").style.top = "-60px"; // Ajusta este valor según la altura de tu barra de navegación
+  }
+  ultimaPosicionScroll = posicionActualScroll;
+};
